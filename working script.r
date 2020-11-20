2fe0fdcccc029ab697d765c5e86a21f51dd2709c
msherga <63604994+msherga@users.noreply.github.com>
2020-11-19 20:40:23 -0600

    v2
    updated/replaced

--- working script.r
+++ working script.r
@@ -32,7 +32,7 @@ setwd('C:/Users/Ronny Sherga/Google Drive/SMU_Masters/Term_2/Stats 2/Project 2/D
 bankaddlfull<-read.csv('bank-additional-full.csv', header = T, sep = ";", stringsAsFactors = T)
 
 #export the df to a standard excel table format so we can quickly and easily investigate more about it.
-write.csv(bankaddlfull, "rewrite.csv", row.names = F)
+#write.csv(bankaddlfull, "rewrite.csv", row.names = F)
 
 #verify vars that should be factors
 tibble(bankaddlfull)
@@ -46,6 +46,11 @@ attach(bankaddlfull)
 #check correlation between pdays and y
 chisq.test(pdays,success)
 ContCoef(pdays, success, correct = T)
+
+#convert pdays to a factor so we can better address the 999. split by weeks.
+bankaddlfull$pdays <- cut(bankaddlfull$pdays, breaks=c(0, 7, 14, 21, 28, Inf),labels=c("1_week", "2_weeks", "3_weeks", "4_weeks", "no_contact"))
+
+
 
 #what are all the levels of the categorical variables?
 sapply((Filter(is.factor,bankaddlfull)), levels)
@@ -70,6 +75,19 @@ bankaddlfull$poutcome[which(poutcome == "nonexistent")] <- NA
 #n/a plot
 gg_miss_var(bankaddlfull)
 
+#add full name months as levels, and change names accordingly
+levels(bankaddlfull$month) <- c(levels(bankaddlfull$month), month.name)
+bankaddlfull$month[which(bankaddlfull$month == "apr")] <- "April"
+bankaddlfull$month[which(bankaddlfull$month == "aug")] <- "August"
+bankaddlfull$month[which(bankaddlfull$month == "dec")] <- "December"
+bankaddlfull$month[which(bankaddlfull$month == "jul")] <- "July"
+bankaddlfull$month[which(bankaddlfull$month == "jun")] <- "June"
+bankaddlfull$month[which(bankaddlfull$month == "mar")] <- "March"
+bankaddlfull$month[which(bankaddlfull$month == "may")] <- "May"
+bankaddlfull$month[which(bankaddlfull$month == "nov")] <- "November"
+bankaddlfull$month[which(bankaddlfull$month == "oct")] <- "October"
+bankaddlfull$month[which(bankaddlfull$month == "sep")] <- "September"
+
 #drop unused levels
 bankaddlfull[] <- lapply(bankaddlfull, function(x) if (is.factor(x)) factor(x) else x)
 sapply((Filter(is.factor,bankaddlfull)), levels)
@@ -81,19 +99,27 @@ table(bankaddlfull$success)
 nrow(bankaddlfull[which(bankaddlfull$pdays == 999),])
 
 #downsample to randomly give sample with same number of Yes/No
-dfNo <- bankaddlfull[which(bankaddlfull$y == "no"),]
-dfYes <- bankaddlfull[which(bankaddlfull$y == "yes"),]
-downsamp <-sample(seq(1:length(dfNo$y)), length(dfYes$y))
-downsamp <- dfNo[downsamp,]
-downsamp <- rbind(downsamp,dfYes)
+# dfNo <- bankaddlfull[which(bankaddlfull$y == "no"),]
+# dfYes <- bankaddlfull[which(bankaddlfull$y == "yes"),]
+# downsamp <-sample(seq(1:length(dfNo$y)), length(dfYes$y))
+# downsamp <- dfNo[downsamp,]
+# downsamp <- rbind(downsamp,dfYes)
+# table(downsamp$y)
 
-table(downsamp$y)
+
 attach(bankaddlfull)
 downdf <- downSample(bankaddlfull, success)
+downdf <- subset(downdf, select = -c(Class))
 
-table(downdf$Class)
+attach(downdf)
 
-#aggregate for summary stats by groups for continous predictors
+#pair plots
+ggpairs(downdf, columns = 1:10, aes(color=success))
+ggpairs(downdf, columns = 11:20, aes(color=success))
+
+#aggregate for summary stats by groups for continous predictors. 
+
+####################################################these are all categorical though. come back to this########################################
 aggregate(education~success,data=downdf,summary) 
 aggregate(job~success,data=downdf,summary) 
 aggregate(marital~success,data=downdf,summary) 
@@ -104,8 +130,10 @@ aggregate(contact~success,data=downdf,summary)
 aggregate(month~success,data=downdf,summary) 
 aggregate(day_of_week~success,data=downdf,summary) 
 aggregate(poutcome~success,data=downdf,summary) 
+aggregate(pdays~success,data=downdf,summary)
 
-#table of counts for categorcal predictors
+
+#table of counts for categorical predictors
 ftable(addmargins(table(education,success))) 
 ftable(addmargins(table(job,success))) 
 ftable(addmargins(table(marital,success))) 
@@ -116,6 +144,7 @@ ftable(addmargins(table(contact,success)))
 ftable(addmargins(table(month,success))) 
 ftable(addmargins(table(day_of_week,success))) 
 ftable(addmargins(table(poutcome,success)))
+ftable(addmargins(table(pdays,success)))
 
 #proportions
 prop.table(table(success,education),2)
@@ -128,6 +157,9 @@ prop.table(table(success,contact),2)
 prop.table(table(success,month),2)
 prop.table(table(success,day_of_week),2)
 prop.table(table(success,poutcome),2)
+prop.table(table(success,pdays),2)
+
+
 
 #visualize proportions for categorical variables
 line_colors <- c("orange", "dodgerblue4")
@@ -138,15 +170,19 @@ plot(success~default,col=line_colors) # No
 plot(success~housing,col=line_colors)
 plot(success~loan,col=line_colors)
 plot(success~contact,col=line_colors) # cellular
-plot(success~month,col=line_colors) # Dec, Sep, Oct, Mar
-plot(success~day_of_week,col=line_colors)
+plot(success~downdf$month,col=line_colors)# not sure why, but needed to specify dataframe even though it's attached
+###reorder day levels###
+downdf$day_of_week <- factor(downdf$day_of_week, levels= c("mon", "tue", "wed", "thu", "fri"))
+plot(success~downdf[order(downdf$day_of_week), ]$day_of_week,col=line_colors)#plot ordered by day of week
 plot(success~poutcome,col=line_colors) # Success
+plot(success~pdays,col=line_colors)
+
+
 
 #boxplots for continuous variables
 plot(age~success,col=line_colors)
 plot(duration~success,col=line_colors)
 plot(campaign~success,col=line_colors)
-plot(pdays~success,col=line_colors) # Need to do something about the 999
 plot(previous~success,col=line_colors)
 plot(emp.var.rate~success,col=line_colors)
 plot(cons.price.idx~success,col=line_colors)
@@ -156,7 +192,7 @@ plot(nr.employed~success,col=line_colors)
 
 #pca analysis on continuous variables
 cont_cols <- c('age','duration','campaign','pdays','previous','emp.var.rate','cons.price.idx','cons.conf.idx','euribor3m','nr.employed','success')
-cont_bankadd <- bankaddlfull[cont_cols]
+cont_bankadd <- downdf[cont_cols]
 pc.result<-prcomp(cont_bankadd[,1:10],scale.=TRUE)
 pc.scores<-pc.result$x
 pc.scores<-data.frame(pc.scores)
@@ -169,12 +205,13 @@ ggplot(data = pc.scores, aes(x = PC1, y = PC2)) +
   scale_color_manual(values=line_colors)
 
 #model
-bankclean<-na.omit(bankaddlfull)
+bankclean<-na.omit(downdf)
 attach(bankclean)
-model.main<-glm(success ~ ., data=bankclean,family = binomial(link="logit"))
+bankclean <- subset(downdf, select = -Class)
+model.main<-glm(success ~ ., data=bankclean,family = binomial(link = "logit"))
 
 #vif
-(vif(model.main)[,3])^2 # not working
+(vif(model.main))^2
 
 #hoslem lack of fit test
 hoslem.test(model.main$y, fitted(model.main), g=10)
